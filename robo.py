f37a9850a68979286cfd640bcf6bad865188b9e5
Shivani Sharma <vistas1202007@gmail.com>
2026-05-31 12:07:06 +0530

    Delete robo.py

--- robo.py
+++
@@ -1,9 +0,0 @@
-import os 
-print("welcomwe to robo speaker 2.0 created by shivani sharma")
-while True :
- x = input("what you want to speak:")
- if x == "q":
-  os.system("say bye bye friend")
-  break
-command =f"say{x}"
-os.system=(command)
